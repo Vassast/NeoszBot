1b92cc856ec89c25ea0d9e59158d35a8c3685150
vassast <vassast@protonmail.com>
2015-11-26 00:34:02 +0100

    Copied files from IrcBot project

---
+++ IrcBot.py
@@ -0,0 +1,41 @@
+import IrcConnection
+import WolfpackRPGEngine
+import MessageHandler
+
+
+class IrcBot:
+	def __init__(self):
+		self._connections 		= list()
+		self._messageHandlers	= list()
+		self._run 				= False
+		
+	def addConnection(self, con):
+		self._connections.append(con)
+	
+	def addMessageHandler(self, handler):
+		self._messageHandlers.append(handler)
+	
+	def run(self):
+		self._run = True
+		for con in self._connections:
+			con.start()
+		try:
+			while self._run:
+				for con in self._connections:
+					data = con.getData()
+					if data:
+						for handler in self._messageHandlers:
+							handler.update(data)
+							replyMessage = handler.getMessage()
+							if replyMessage:
+								print('Sent message: ' + replyMessage[0] + ' ' + replyMessage[1])
+								con.sendMessage(replyMessage[0] + ' ' + replyMessage[1])
+			
+			for con in self._connections:
+				con.join()
+		except KeyboardInterrupt:
+				for con in self._connections:
+					con.logout()
+				self._run = False
+				for con in self._connections:
+					con.join()
---
+++ PingHandler.py
@@ -0,0 +1,9 @@
+import MessageHandler
+
+class PingHandler(MessageHandler.MessageHandler):
+	def __init__(self):
+		super().__init__()
+		
+	def update(self, data):
+		if data[1] == 'PING':
+			self.addMessage(('PONG', data[2][0].replace('\r\n', '')))
